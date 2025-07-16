0db68dfdb420676f1f8e56ff1863d420627e0268
wrenstuff <stewart.nzwren@gmail.com>
2025-07-16 12:09:15 +1200

    Add database status page and update app routing
    Introduces a new db-status.html template to display all database entries for Events, Races, Religions, Settlements, and Worlds. Updates app.py to query these models and render the new template at the root route, changes the database URI, and removes a compiled Python cache file. Also adds /instance to .gitignore.

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 from models import db
-from flask import Flask
+from flask import Flask, render_template
+
+from models import Events
+from models import Races
+from models import Religions
+from models import Settlements
+from models import Worlds
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = 'secret_key'
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-def index():
-    return "Welcome to the World Building Manager!"
+def DB_Status():
+    events = Events.query.all()
+    races = Races.query.all()
+    religions = Religions.query.all()
+    settlements = Settlements.query.all()
+    worlds = Worlds.query.all()
+
+    return render_template('db-status.html', 
+                           events=events, 
+                           races=races, 
+                           religions=religions, 
+                           settlements=settlements, 
+                           worlds=worlds)
 
 if __name__ == '__main__':
     app.run(debug=True)
